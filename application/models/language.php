30675dc002f908939aa757d58a18d2486309109e
Rohit Khattar <rohitkh@byu.edu>
2021-03-09 12:20:52 -0700

    Updated Language connector

--- application/models/language.php
+++ application/models/language.php
@@ -1,22 +1,18 @@
 <?php
-class Language extends CI_Model
-{
-	
-	function __construct()
-	{
-		$this->DB1 = 
-		
-		$mysqlserver="worldwater.byu.edu";
-		$mysqlusername="langreader";
-		$mysqlpassword="readHSLlang@9";
-		$this->DB1 =@mysqli_connect($mysqlserver, $mysqlusername, $mysqlpassword);
-		$dbname = 'hydroserver_translation';
+class Language extends CI_Model {
+
+	function __construct() {
+
+		$mysqlserver   = "128.187.106.131";
+		$mysqlusername = "langreader";
+		$mysqlpassword = "readHSLlang@9";
+		$this->DB1     = @mysqli_connect($mysqlserver, $mysqlusername, $mysqlpassword);
+		$dbname        = 'hydroserver_translation';
 		@mysqli_select_db($this->DB1, $dbname);
-		
+
 	}
-	function getTerms($lang)
-	{
-		$sql = "SELECT * FROM hydroserver_translation.translations_by_language";
+	function getTerms($lang) {
+		$sql   = "SELECT * FROM hydroserver_translation.translations_by_language";
 		$terms = mysqli_query($this->DB1, $sql);
 		return $terms;
 	}
